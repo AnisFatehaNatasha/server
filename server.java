d55caf2c9e16894edb0f35a0e950e973b77be7ec
anis fateha natasha ardhi <48323125+AnisFatehaNatasha@users.noreply.github.com>
2019-03-20 15:53:39 +0800

    Update server.java

--- server.java
+++ server.java
@@ -5,7 +5,7 @@ public class server{
     public static void main(String args[]) throws Exception {
         String clientSentence;
         String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(6789);
+        ServerSocket welcomeSocket = new ServerSocket(5000);
 
         while(true) {
             Socket connectionSocket = welcomeSocket.accept();
